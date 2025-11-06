a0a8ddcaefbe64c0a9afc043bb94781dab141a8b
abdulholiq-ahmad <abdulholiq571@gmail.com>
2025-11-06 20:55:31 +0500

    94.binary_tree_inorder_traversal

---
+++ 88.merge_sorted_array/index.js
@@ -0,0 +1,17 @@
+var merge = function(nums1, m, nums2, n) {
+    let i = nums1.length - 1
+    let j = nums2.length - 1
+    let k = nums1.length + nums2.length - 1
+
+    console.log(i,j,k)
+
+    while(i >= 0 && j >= 0) {
+
+    }
+
+    while (j >= 0) {
+    // Faqat nums2 da element qolgan
+    }
+};
+
+merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
